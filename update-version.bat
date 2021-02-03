eb4b6252eef65987fe970c95594427d63e920b0c
xingwanxiang <xingwanxiang@autohome.com.cn>
2021-02-03 16:05:25 +0800

    version 1.1.3

--- update-version.bat
+++ update-version.bat
@@ -1,5 +1,5 @@
 export GPG_TTY=$(tty)
-mvn versions:set -DnewVersion=1.1.0-RC2 -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=1.1.3 -DgenerateBackupPoms=false
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
 
 mvn versions:commit
