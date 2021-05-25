01e9c9eaf16161ac34c82a920919df56266d63b8
xingwanxiang <xingwanxiang@autohome.com.cn>
2021-05-25 20:03:48 +0800

    fix javadoc error

--- c-jmesa/src/com/bixuebihui/jmesa/EasyTable.java
+++ c-jmesa/src/com/bixuebihui/jmesa/EasyTable.java
@@ -141,7 +141,7 @@ public class EasyTable extends BasicWebUI {
     }
 
     /**
-     * use {@see LimitActionFactoryJsonImpl}
+     * use @see LimitActionFactoryJsonImpl
      * json format { "id": "table-id", "action": "", "maxRows": 500, "page": 123,
      * "filter": { "property1":value1, "property2":value2, "property3": [fromValue, toValue]},
      * "sort":{ "property1":"asc", "property2":"desc", },
