238db496336c0475b4ccdda92d35bc404d69eff2
zhangmengfei <Mf@#2021>
2021-07-30 12:42:12 +0800

    增加mvn打包命令

--- update-version.bat
+++ update-version.bat
@@ -8,3 +8,11 @@ mvn clean deploy -Ppro
 
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0 versions:commit
+
+
+mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}.1 versions:commit
+
+
+mvn build-helper:parse-version versions:set -DnewVersion=1.4.4.3 versions:commit
+
+mvn -DskipTests=true deploy -Ppro
